e23e4991ea8f081ae158fa58e3aae7b16bf50ba8
harikaneethu <74321961+harikaneethu@users.noreply.github.com>
2025-05-03 01:01:19 +0530

    Add files via upload

---
+++ class5-3.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the first number:"))
+m=int(input("Enter the second number:"))
+if( n>m ):
+  print("The first number is greater than the second number")
+else:
+  print("The second number is greater than the first number")
